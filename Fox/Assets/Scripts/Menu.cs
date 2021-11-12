f74e9e21ae44f5acdfd9f1faa7c605ec5c70dfe8
xxxxcABC <93467250+xxxxcABC@users.noreply.github.com>
2021-11-12 16:17:35 +0800

    11.12 16:16
    feat(scrpts/Menu/SetVolume):新增调节音效
    添加滑动条滑动调节音效大小

--- Fox/Assets/Scripts/Menu.cs
+++ Fox/Assets/Scripts/Menu.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
 public class Menu : MonoBehaviour
 {
     public GameObject PauseMenu;
+    public AudioMixer AdMixer;
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -30,6 +31,10 @@ public class Menu : MonoBehaviour
     {
         PauseMenu.SetActive(false);
         Time.timeScale = 1f;
+    }
+    public void SetVolume(float value)
+    {
+        AdMixer.SetFloat("MainVolume", value);
     }
 
 }
